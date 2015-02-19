02a23318f8296817ac84d78b888e46f564ce0e55
Tom Molesworth <tom@perlsite.co.uk>
2015-02-19 15:24:35 +0000

    Use SPDY/3.1 by default and test some things

--- roundtrip.pl
+++ roundtrip.pl
@@ -1,23 +1,41 @@
-#!/usr/bin/env perl;
+#!/usr/bin/env perl
 use strict;
 use warnings;
 
+use Log::Any qw($log);
+use Log::Any::Adapter qw(TAP);
+
 use Test::More;
+use Test::Fatal;
 
 use Web::Async;
 
 my $loop = IO::Async::Loop->new;
+
+# Set up a server first
 $loop->add(
-	my $srv = new_ok('Web::Async::Server')
-);
-$loop->add(
-	my $cli = new_ok('Web::Async::Client')
+	my $srv = new_ok('Web::Async::Server', [
+		protocols => [
+			'spdy/3.1'
+		]
+	])
 );
 my ($host, $port) = $srv->listen->get;
 note "Listen on host $host:$port";
 
+# Make a simple GET request with a new client instance
+$loop->add(
+	my $cli = new_ok('Web::Async::Client')
+);
 my $resp = $cli->GET(
-	'https://' . $host . ':' . $port
+	'https://' . $host . ':' . $port . '/some/page.html?x=123&y=abcd'
 )->get;
+isa_ok($resp, 'HTTP::Response');
+ok($resp->is_success, 'had success');
+is($resp->code, 200, 'status code 200');
+is($resp->protocol, 'HTTP/1.1', 'correct protocol');
+note $resp->as_string("\n");
+
+$loop->run;
 
 done_testing;
