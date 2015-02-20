0d137114d45ed30a5829b4201c701ca9ffcf2d17
Tom Molesworth <tom@perlsite.co.uk>
2015-02-20 01:59:42 +0000

    Notes about limits

--- lib/Web/Async/Client.pm
+++ lib/Web/Async/Client.pm
@@ -7,6 +7,58 @@ use parent qw(IO::Async::Notifier);
 
 # Usage: perl client-async.pl https://spdy-test.perlsite.co.uk/index.html
 
+=pod
+
+=head2 Request limits
+
+max_connections_per_ip - total number of active TCP connections allowed to the same resolved IP address
+
+max_connections_per_host - total number of active TCP connections allowed to the same host:port combination
+
+max_requests_per_host - total number of active HTTP requests allowed for a host:port combination
+
+The connection limits restrict how many active TCP connections we will maintain. Since protocols often
+have the ability to have multiple active requests on the same connection, the request limits may be more
+useful if the intention is to limit the load on the target server.
+
+To ensure that we only ever have 4 unfinished requests in progress for any given host, use:
+
+ max_requests_per_host => 4
+
+This will queue any additional requests in first-in, first-out order. You can examine the current queue
+status using:
+
+ $client->request_queue
+
+to retrieve all requests that have not yet been started, and
+
+ $client->request_queue_for(host => 'www.example.com')
+
+to limit this to a specific server.
+
+Global limits are also available:
+
+ max_total_requests => 32
+
+This will restrict to 32 total requests across all servers.
+
+=head2 Queue policy
+
+There is some degree of control over how requests will be added to the queue:
+
+ queue_policy => 'fifo'|'lifo'|'fair'|sub { ... }
+
+The default is 'fifo', which means that it's possible to have a large number of requests in
+the queue for one host that will need to be processed before requests for other hosts are entertained.
+
+=head2 Bandwidth limits
+
+=head2 Accounting
+
+One side effect of the bandwidth controls is that we are able to track and report usage.
+
+=cut
+
 use Log::Any qw($log);
 
 use Protocol::SPDY;
